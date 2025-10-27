c04aaeb8b51f1c4e91f2d771ac2ecf0f17565caa
Marouane Bachar <marstrone@gmail.com>
2025-10-27 13:16:35 +0100

    count all chars nums sym but no spaces

--- script.js
+++ script.js
@@ -4,5 +4,13 @@ const countWords=(sentence)=> {
     return countWords.length;
 }
 
-
 console.log(countWords("hello world Marstrx"));
+
+// count all chars nums sym but no spaces
+const countAllThingsExSpcs=(sentence)=>{
+    const removeSpaces = sentence.replaceAll(" ","");
+    const allThingsExSpcs = removeSpaces.length;
+    return allThingsExSpcs;
+}
+
+console.log(countAllThingsExSpcs("hello world Marstrx"));
