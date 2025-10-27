f0a46d1d69e1a37e6e37c596674eccb941ce3b90
Marouane Bachar <marstrone@gmail.com>
2025-10-27 13:25:01 +0100

    count numbers in the sentence

--- script.js
+++ script.js
@@ -14,3 +14,18 @@ const countAllThingsExSpcs=(sentence)=>{
 }
 
 console.log(countAllThingsExSpcs("hello world Marstrx"));
+
+// count numbers in the sentence
+
+const countNumbers=(sentence)=>{
+    const numbers = "0123456789";
+    let count =0;
+    for(const chars of sentence){
+        if(numbers.includes(chars)){
+            count++;
+        }
+    }
+    return count ;
+}
+
+console.log(countNumbers("hello world Marstrx2025"));
